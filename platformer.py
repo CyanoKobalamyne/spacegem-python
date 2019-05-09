1e4e6ef9cc3348ec12b47fcaeb97b1927dd9987d
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-05-09 12:56:23 -0400

    Add basic movement

--- platformer.py
+++ platformer.py
@@ -4,10 +4,14 @@ from numbers import Number
 
 import pygame
 import pygame.display
+import pygame.event
 import pygame.sprite
 import pygame.time
 from pygame import Color, Surface
-from pygame.sprite import Group, GroupSingle, Sprite
+from pygame.sprite import Group, Sprite
+
+
+FPS = 60
 
 
 class Vector(namedtuple('Vector', ['x', 'y'])):
@@ -23,7 +27,7 @@ class Vector(namedtuple('Vector', ['x', 'y'])):
         return Vector(x, y)
 
     def __sub__(self, other):
-        self.__add__(-other)
+        return self.__add__(-other)
 
     def __mul__(self, other):
         if isinstance(other, Number):
@@ -34,34 +38,55 @@ class Vector(namedtuple('Vector', ['x', 'y'])):
     def __rmul__(self, other):
         return self.__mul__(other)
 
+    def __truediv__(self, other):
+        return self.__mul__(1 / other)
+
 
 class NotePlatformerScene:
     def __init__(self):
-        self.player = GroupSingle()
         self.platforms = Group()
         self.enemies = Group()
-        self.objects = [self.platforms, self.enemies, self.player]
+        self.blobs = Group()
 
-        self.load()
+        platforms = [
+            Platform(position=Vector(0, 50)),
+            Platform(position=Vector(110, 50)),
+            Platform(position=Vector(220, 50)),
+        ]
+        enemies = [
+            Enemy(position=Vector(100, 40)),
+        ]
+        self.player = Player(position=Vector(40, 40))
 
-    def load(self):
-        self.player.add(Player(position=Vector(40, 40)))
-        self.platforms.add(Platform(position=Vector(0, 50)))
-        self.platforms.add(Platform(position=Vector(110, 50)))
-        self.platforms.add(Platform(position=Vector(220, 50)))
-        self.enemies.add(Enemy(position=Vector(100, 40)))
+        for blob in platforms:
+            self.blobs.add(blob)
+            self.platforms.add(blob)
+        for blob in enemies:
+            self.blobs.add(blob)
+            self.enemies.add(blob)
+        self.blobs.add(self.player)
 
     def render(self, screen):
         screen.fill(Color('white'))
-        for sprite_group in self.objects:
-            sprite_group.draw(screen)
+        self.blobs.draw(screen)
 
     def update(self):
-        for sprite_group in self.objects:
-            sprite_group.update()
+        self.blobs.update()
 
     def handle_events(self, events):
-        pass
+        for event in events:
+            print(event)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.go_left()
+                if event.key == pygame.K_RIGHT:
+                    self.player.go_right()
+                if event.key == pygame.K_UP:
+                    self.player.jump()
+ 
+            if event.type == pygame.KEYUP:
+                if event.key in {pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP}:
+                    self.player.stop()
 
 
 class Blob(Sprite):
@@ -70,11 +95,13 @@ class Blob(Sprite):
         self.image = Surface((width, height))
         self.image.fill(Color(color))
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = position
+        self.position = position
         self.velocity = velocity
 
     def update(self):
-        self.rect.move_ip(*self.velocity)
+        self.position += self.velocity / FPS
+        pos_diff = self.position - Vector(self.rect.x, self.rect.y)
+        self.rect.move_ip(*pos_diff)
 
 
 class Platform(Blob):
@@ -84,7 +111,21 @@ class Platform(Blob):
 
 class Player(Blob):
     def __init__(self, **kwargs):
-        super().__init__(10, 10, 'green', velocity=Vector(20, 0), **kwargs)
+        super().__init__(10, 10, 'green', **kwargs)
+        self.v_run = Vector(20, 0)
+        self.v_jump = Vector(0, -5)
+
+    def go_left(self):
+        self.velocity = -self.v_run
+
+    def go_right(self):
+        self.velocity = self.v_run
+
+    def jump(self):
+        self.velocity = self.v_jump
+
+    def stop(self):
+        self.velocity = Vector(0, 0)
 
 
 class Enemy(Blob):
@@ -96,20 +137,21 @@ def main():
     pygame.init()
 
     size = [600, 400]
-    screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+    screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Test platformer')
 
     level = NotePlatformerScene()
     clock = pygame.time.Clock()
-    fps = 2
 
     while True:
-        if any(event.type == pygame.QUIT for event in pygame.event.get()):
+        events = pygame.event.get()
+        if any(event.type == pygame.QUIT for event in events):
             break
 
+        level.handle_events(events)
         level.update()
         level.render(screen)
-        clock.tick(fps)
+        clock.tick(FPS)
         pygame.display.flip()
 
     pygame.quit()
